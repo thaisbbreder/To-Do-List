ae3dfc8a06e848d5159241637181dbbd90ebbd72
unknown <thaisbragabreder@gmail.com>
2022-10-24 14:55:28 -0300

    inseri botão de urgencia

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,19 +9,18 @@ import {
 } from "@mui/material";
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import MapIcon from '@mui/icons-material/Map';
 import Grid2 from '@mui/material/Unstable_Grid2';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
 import { useEffect, useState } from "react";
 
 const Home = () => {
   const [tarefas, setTarefas] = useState([]);
-  const [textoTarefa, setTextoTarefa] = useState("") 
-  const [prioridade, setPrioridade] = useState('');
+  const [textoTarefa, setTextoTarefa] = useState("")
 
   const addTarefa = () => {
     const novoArray = [...tarefas];
@@ -29,13 +28,12 @@ const Home = () => {
       id: tarefas.length + 1,
       nome: textoTarefa,
       descricao: textoDescricao,
-      prioridade: false,
-      feito: false,
+      prioridade: false, //false: normal  true: urgente
+      feito: false, //false: não feito true: feito
     }
     novoArray.push(novaTarefa);
     setTarefas(novoArray)
   }
-
   const [textoDescricao, setTextoDescricao] = useState("") //variavel para armazenar o valor do input
   const addDescricao = () => {
     const itemDescricao = [...tarefas.descricao];
@@ -43,70 +41,76 @@ const Home = () => {
       id: tarefas.length + 1,
       nome: textoTarefa,
       descricao: textoDescricao,
-      prioridade: false,
       feito: false,
     }
     itemDescricao.push(novaDescricao);
     setTarefas(itemDescricao)
   }
-
   const deletaTarefa = (id) => {
     const novaLista = tarefas.filter((tarefa) => {
       return tarefa.id !== id
     })
     setTarefas(novaLista)
   }
-
-  //quando eu clico no checkbox, o estado atual da tarefa é alterado para um novo estado (feito: true/false) 
-  //e o código é refatorado/refeito cada vez que o estado é alterado, 
-  //fazendo com que seja possivel marcar o checkbox de cada uma das tarefas e não perder o que foi feito no estado anterior
   const mudaEstado = (id) => {
     setTarefas((estadoAtual) => {
       const novoEstado = [...estadoAtual];
-      //spread operator: está fazendo uma cópia do estado atual 
-      //ao clicar em algum checkbox, o filter vai percorrer o array até identificar o id referente a essa tarefa e retornar um valor baseado na condição
       novoEstado.filter((tarefa) => {
         if (tarefa.id === id) {
-          tarefa.feito = !tarefa.feito;   //se o "feito" estiver recebendo false, agora ele irá receber o contrário (true)
-          return tarefa;
+          tarefa.feito = !tarefa.feito;
         } else {
           return tarefa;
         }
       });
       return novoEstado;
-    });
+    })
   };
-
-   const mudaPrioridade = (id) => { 
-       }
+  //clicar no botão para a prioridade ser alterada
+  const mudaPrioridade = (id) => {
+    setTarefas((prioridadeAtual) => {
+      const novaPrioridade = [...prioridadeAtual];
+      novaPrioridade.filter((tarefa) => {
+        if (tarefa.id === id) {
+          tarefa.prioridade = !tarefa.prioridade;
+          tarefas.sort()
+          return tarefa;
+        } else {
+          return tarefa;
+        }
+      });
+      return novaPrioridade;
+    })
+  }
 
   return (
-    <Grid2 container spacing={1}>
+    <Grid2 container spacing={2}>
       <div
         style={{
           display: "grid",
           width: "100vw",
           height: "100vh",
-          backgroundColor: "#3b3b3b",
+          background: "linear-gradient(to bottom right, #007e80 20%, #004853 90%",
         }}
       >
-        <Paper elevation={3}
+        <Paper
           style={{
             display: "grid",
             margin: "auto",
-            width: "30%",
+            width: "40%",
             maxHeight: "60%",
             padding: "40px",
-            overflow: "auto"
+            overflow: "auto",
+            textAlign: "center",
+            color: "#007e80"
           }}
         >
           <Grid2 xs={12}>
-            <Typography fontSize={24} fontWeight={"bold"} textAlign={"center"} >
+            <Typography fontSize={34} fontWeight={"bold"} textAlign={"center"} >
               To-do List
             </Typography>
           </Grid2>
 
-          <Grid2 xs={12} textAlign={"center"}>
+          <Grid2 xs={12}>
             <TextField
               id="outlined-basic"
               label="Nova tarefa"
@@ -117,7 +121,6 @@ const Home = () => {
               value={textoTarefa}
               onChange={e => setTextoTarefa(e.target.value)}
             />
-
             <TextField
               id="outlined-basic"
               label="Descrição"
@@ -128,8 +131,7 @@ const Home = () => {
               value={textoDescricao}
               onChange={e => setTextoDescricao(e.target.value)}
             />
-
-            <Button variant="contained" 
+            <Button variant="contained" color="secondary" margin="dense"
               onClick={() => addTarefa()}>
               Adicionar
             </Button>
@@ -138,50 +140,56 @@ const Home = () => {
           {tarefas.map((tarefa) => {
             return (
               <div key={tarefa.id}>
-                <Grid2 xs={12} display="flex" 
-                flexDirection="row" 
-                justifyContent="space-between" 
-                alignItems="center" style={{backgroundColor: tarefa.prioridade ? "#e1d772" : "#f06b50" }}>
-
+                <Grid2 xs={12}
+                  display="flex"
+                  flexDirection="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  borderRadius="5px"
+                  style={{
+                    backgroundColor: tarefa.prioridade === false ? "" : "#ffbfaa",
+                  }}
+                >
                   <FormControlLabel
                     label={tarefa.nome}
                     control={
-                      <Checkbox 
+                      <Checkbox
                         checked={tarefa.feito}
-                        onChange={() => mudaEstado(tarefa.id)}>
+                        onChange={() => mudaEstado(tarefa.id)}
+                      >
                       </Checkbox>
                     }>
                   </FormControlLabel>
-                                            
-                    <Typography>
-                      {tarefa.descricao}
-                    </Typography>
 
-                    <Box sx={{ minWidth: 120 }}>
-                  <FormControl variant="standard" size="small">
-                    <InputLabel id="demo-simple-select-label">Priority</InputLabel>
-                    <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      value={prioridade}
-                      label="Priority"
-                      onChange={e => setPrioridade(e.target.value)} 
-                    >
-                      <MenuItem value={10}>Normal</MenuItem>
-                      <MenuItem value={20}>Low</MenuItem>
-                      <MenuItem value={30}>High</MenuItem>
-                    </Select>
-                  </FormControl>
-                  </Box>
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    size="small"
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-end",
+                      color: "#007e80"
+                    }}
+                    onClick={() => mudaPrioridade(tarefa.id)}> urgente
+                  </Button>
 
-                    <IconButton aria-label="delete"
+                  <IconButton aria-label="delete"
+                    style={{
+                      textAlign: "left",
+                      color: "#007e80",
+                      position: "relative",
+                      textAlign: "left"
+                    }}
                     onClick={() => deletaTarefa(tarefa.id)}>
                     <DeleteIcon />
-                  </ IconButton>              
+                  </ IconButton>
 
+                  <Typography textAlign={"left"}>
+                    {tarefa.descricao}
+                  </Typography>
                 </Grid2>
-
                 <Divider />
+
               </div>
             );
           })}
