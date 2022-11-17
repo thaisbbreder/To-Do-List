048961eafbae1cb799bb6109b01feddedbefcdab
unknown <thaisbragabreder@gmail.com>
2022-11-17 11:23:36 -0300

    insere página de login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
 import Home from "./pages/home";
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider,BrowserRouter as Router, Routes, Route, Form} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home></Home>,
-  },
-]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home></Home>,
+    },  
+  ]); 
+  function App() {
+  return (
+   <RouterProvider router={router} /> 
+   ) 
+  }
+  
+  export default App;
+
+
+  {/* 
+import { app } from './firebase-config';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const handleAction = (id) => {
+    const authentication = getAuth();
+    if (id === 2) {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          console.log(response)
+      })
+   }
+  return (
+<Routes>
+            <Route
+              path='/login'
+              element={
+                <Form
+                  title="Login"
+                  setEmail={setEmail}
+                  setPassword={setPassword}
+                  handleAction={() => handleAction()}
+                />}
+            />
+            <Route
+              path='/register'
+              element={
+                <Form
+                  title="Register"
+                  setEmail={setEmail}
+                  setPassword={setPassword}
+                  handleAction={() => handleAction()}
+                />}
+            />
+          </Routes>
+  );
 }
+export default App; */}
 
-export default App;
+
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,19 +18,22 @@ import {
 } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
+import GoogleIcon from "@mui/icons-material/Google";
 import EditIcon from "@mui/icons-material/Edit";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import { useEffect, useState } from "react";
 import { signInWithPopup, signOut } from "firebase/auth";
 import { auth, provider } from "../services/databaseService";
+import Todo1 from ".//todo1.png";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Home = () => {
   // Estados: usados para alterar as variáveis. Retorna um valor e uma função para alterar esse valor
   const [tarefas, setTarefas] = useState([]); //referente a lista
   const [textoTarefa, setTextoTarefa] = useState(""); // referente ao valor inserido no Nova Tarefa
   const [textoDescricao, setTextoDescricao] = useState(""); //referente ao valor inserido no Descrição
+  const [editTarefa, setEditTarefa] = useState(0);
   const [urgencia, setUrgencia] = useState(1); // referente ao campo Urgencia
-  
 
   const cores = {
     1: "#F3E6BC",
@@ -99,24 +102,86 @@ const Home = () => {
   };
 
   // clicar no Edit > identificar o id da tarefa > enviar o nome/descrição para o form
+  //criar um novo estado para receber o valor da tarefa que está sendo alterada no form
   //find: retorna o valor do 1º elemento que passar pelo teste. Parece o filter mas retorna apenas o primeiro elemento que passa pelo teste e não o array inteiro
   const mudaTarefa = (id) => {
     const editaTarefa = tarefas.find((tarefa) => tarefa.id === id);
     setTextoTarefa(editaTarefa.textoTarefa);
     setTextoDescricao(editaTarefa.textoDescricao);
+    setEditTarefa(id);
   };
-  //criar um novo estado para receber o valor da tarefa que está sendo alterada no form
 
   useEffect(() => {
     if (!auth.currentUser) {
       console.log("Não está logado");
-      //pode enviar para uma página diferente de quando esta logado
     } else {
       console.log("Está logado");
     }
   }, []);
 
-  return (
+// PÁGINA DO LOGIN
+    return (
+    <Grid2
+      container
+      spacing={4}
+      style={{
+        display: "grid-inline",
+        width: "100vw",
+        height: "100vh",
+        background: "linear-gradient(136deg, #B8CAD4 10%, #cdcdcd 80%)",
+        overflow: "hidden",
+      }}
+    >
+      <Paper
+        elevation={3}
+        style={{
+          margin: "auto",
+          maxWidth: "50%",
+          height: "70%",
+          padding: "30px",
+          textAlign: "center",
+        }}
+      >
+        <Grid2 xs={12}>
+          <Typography
+            fontSize={70}
+            fontWeight={"bold"}
+            color={"#0093E9"}
+            style={{
+              fontFamily: "Fjalla One",
+              textAlign: "center",
+              margin: "10px",
+            }}
+          >
+            To-do list
+          </Typography>
+        </Grid2>
+
+        <Grid2 xs={12}>        
+          <Button
+            variant="contained"
+            startIcon={<GoogleIcon />}
+            fullWidth
+            style={{ marginTop: "15px" }}
+            onClick={() =>
+              signInWithPopup(auth, provider).then((result) => {
+                console.log(result);
+              })
+            }
+          >
+            Login with google
+          </Button>
+        </Grid2>
+      </Paper>
+      <img
+        src={Todo1}
+        alt="Imagem Todo"
+        style={{ margin: "auto", width: "50%" }}
+      />
+    </Grid2>
+  
+
+  /* return (
     <Grid2
       container
       spacing={4}
@@ -124,35 +189,41 @@ const Home = () => {
         display: "grid",
         width: "100vw",
         height: "100vh",
-        background: "linear-gradient(to bottom right, #007e80 20%, #004853 90%",
+        background: "linear-gradient(136deg, #B8CAD4 10%, #cdcdcd 80%)",
       }}
     >
-      
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+      <Box>
+        <AppBar position="static" sx={{ backgroundColor: "primary.dark" }}>
           <Toolbar>
-            {/*<IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-               <Avatar
-                src={auth.currentUser.photoURL}
-                referrerpolicy="no-referrer"
-              />
-            </IconButton>
-
-            <Typography sx={{ flexGrow: 1 }}>
-            {"Seja bem vindo(a), " + auth.currentUser.displayName + "!"} */}
-             <Typography sx={{ flexGrow: 1 }}>
-            Seja bem vindo(a)
+            /*<Avatar
+              src={auth.currentUser.photoURL}
+              referrerPolicy="no-referrer"
+              style={{ margin: "25px 10px 5px 0" }}
+            /> 
+            <Typography
+              sx={{ flexGrow: 1 }}
+              fontWeight={"bold"}
+              style={{ margin: "20px 0 5px 0" }}
+            > 
+            Seja bem vindo!
+             // {"Seja bem vindo, " + auth.currentUser.displayName + "!"} 
             </Typography>
             <Button
               variant="contained"
-              color="secondary"
               size="small"
+              style={{ margin: "20px 10px 5px 0" }}
+              onClick={() =>
+                signInWithPopup(auth, provider).then((result) => {
+                  console.log(result);
+                })
+              }
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              size="small"
+              style={{ margin: "20px 10px 5px 0" }}
               onClick={() =>
                 signOut(auth).then(() => {
                   console.log("deslogado");
@@ -165,173 +236,151 @@ const Home = () => {
         </AppBar>
       </Box>
 
-      {auth.currentUser &&
-      <Paper
-        style={{
-          margin: "auto",
-          width: "40%",
-          maxHeight: "60%",
-          padding: "40px",
-          overflow: "auto",
-        }}
-      >
-        <Grid2 xs={12}>
-          <Typography
-            fontSize={34}
-            fontWeight={"bold"}
-            color={"#007E80"}
-            textAlign={"center"}
-          >
-            To-do List
-          </Typography>
-          {/*
-         {"To-do List de " + auth.currentUser.displayName}
-          </Typography> */}
-        </Grid2>
+      {auth.currentUser && (
+        <Paper
+          style={{
+            margin: "auto",
+            width: "40%",
+            maxHeight: "80%",
+            padding: "30px",
+            overflow: "auto",
+          }}
+        >
+          <Grid2 xs={12}>
+            <Typography
+              fontSize={50}
+              fontWeight={"bold"}
+              color={"#0093E9"}
+              textAlign={"center"}
+              style={{ fontFamily: "Fjalla One" }}
+            > To-do List
+            //{"To-do list de " + auth.currentUser.displayName}
+            </Typography>
+          </Grid2>
 
-        <Grid2 xs={12} textAlign="center">
-          <FormControl fullWidth>
-            <TextField
-              id="outlined-basic"
-              label="Nova tarefa"
-              variant="outlined"
-              size="small"
-              fullWidth
-              value={textoTarefa}
-              onChange={(e) => setTextoTarefa(e.target.value)}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <TextField
-              id="outlined-basic"
-              label="Descrição"
-              variant="outlined"
-              size="small"
-              margin="dense"
-              fullWidth
-              value={textoDescricao}
-              onChange={(e) => setTextoDescricao(e.target.value)}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Urgência</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              label="Urgência"
-              size="small"
-              value={urgencia}
-              onChange={(e) => setUrgencia(e.target.value)}
+          <Grid2 xs={12} textAlign="center">
+            <FormControl fullWidth>
+              <TextField
+                id="outlined-basic"
+                label="Nova tarefa"
+                variant="outlined"
+                size="small"
+                fullWidth
+                value={textoTarefa}
+                onChange={(e) => setTextoTarefa(e.target.value)}
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField
+                id="outlined-basic"
+                label="Descrição"
+                variant="outlined"
+                size="small"
+                fullWidth
+                style={{ margin: "10px 0 10px 0" }}
+                value={textoDescricao}
+                onChange={(e) => setTextoDescricao(e.target.value)}
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Urgência</InputLabel>
+              <Select
+                id="demo-simple-select"
+                label="Urgência"
+                size="small"
+                value={urgencia}
+                onChange={(e) => setUrgencia(e.target.value)}
+              >
+                {[1, 2, 3, 4, 5].map((item) => (
+                  <MenuItem key={"urgencia-" + item} value={item}>
+                    {item}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Button
+              variant="contained"
+              //color="secondary"
+              style={{ margin: "10px 0 0 0" }}
+              onClick={() => addTarefa()}
             >
-              {[1, 2, 3, 4, 5].map((item) => (
-                <MenuItem key={"urgencia-" + item} value={item}>
-                  {item}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <Button
-            variant="contained"
-            color="secondary"
-            style={{ margin: "10px 0 0 0" }}
-            onClick={() => addTarefa()}
-          >
-            Adicionar
-          </Button>
-        </Grid2>
+              Adicionar
+            </Button>
+          </Grid2>
 
-        {tarefas.map((tarefa) => {
-          return (
-            <Grid2
-              container
-              key={tarefa.id}
-              alignItems="center"
-              style={{
-                backgroundColor: cores[tarefa.urgencia],
-                textDecoration: tarefa.feito === false ? "" : "line-through",
-                maxHeight: "100px",
-                padding: "0 10px",
-                margin: "10px 10px 0 0",
-              }}
-              spacing={0}
-              borderRadius="5px"
-            >
-              <Grid2 item sm={1}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={tarefa.feito}
-                      onChange={() => mudaEstado(tarefa.id)}
-                    ></Checkbox>
-                  }
-                ></FormControlLabel>
-              </Grid2>
+          {tarefas.map((tarefa) => {
+            return (
+              <Grid2
+                container
+                key={tarefa.id}
+                alignItems="center"
+                style={{
+                  backgroundColor: cores[tarefa.urgencia],
+                  textDecoration: tarefa.feito === false ? "" : "line-through",
+                  maxHeight: "100px",
+                  padding: "0 10px",
+                  margin: "10px 10px 0 0",
+                }}
+                spacing={0}
+                borderRadius="5px"
+              >
+                <Grid2 item sm={1}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={tarefa.feito}
+                        onChange={() => mudaEstado(tarefa.id)}
+                      ></Checkbox>
+                    }
+                  ></FormControlLabel>
+                </Grid2>
 
-              <Grid2 item sm={6}>
-                <Typography p={"10px 0 2px"} fontWeight={"bold"}>
-                  {tarefa.nome}
-                </Typography>
-                <Typography p={"2px 0 10px"}>{tarefa.descricao}</Typography>
-              </Grid2>
+                <Grid2 item sm={6}>
+                  <Typography p={"10px 0 2px"} fontWeight={"bold"}>
+                    {tarefa.nome}
+                  </Typography>
+                  <Typography p={"2px 0 10px"}>{tarefa.descricao}</Typography>
+                </Grid2>
 
-              <Grid2 item sm={5} textAlign={"right"}>
-                <FormControl>
-                  <Select
-                    style={{ borderRadius: "100px" }}
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    size="small"
-                    value={tarefa.urgencia}
-                    onChange={(e) => mudaUrgencia(tarefa.id, e.target.value)}
+                <Grid2 item sm={5} textAlign={"right"}>
+                  <FormControl>
+                    <Select
+                      style={{ borderRadius: "100px" }}
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      size="small"
+                      value={tarefa.urgencia}
+                      onChange={(e) => mudaUrgencia(tarefa.id, e.target.value)}
+                    >
+                      {[1, 2, 3, 4, 5].map((item) => (
+                        <MenuItem key={item} value={item}>
+                          {item}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+
+                  <IconButton
+                    aria-label="edit"
+                    onClick={() => mudaTarefa(tarefa.id)}
                   >
-                    {[1, 2, 3, 4, 5].map((item) => (
-                      <MenuItem key={item} value={item}>
-                        {item}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
+                    <EditIcon />
+                  </IconButton>
 
-                <IconButton
-                  aria-label="edit"
-                  style={{
-                    color: "#007e80",
-                  }}
-                  onClick={() => mudaTarefa(tarefa.id)}
-                >
-                  <EditIcon />
-                </IconButton>
-
-                <IconButton
-                  aria-label="delete"
-                  style={{
-                    color: "#007e80",
-                  }}
-                  onClick={() => deletaTarefa(tarefa.id)}
-                >
-                  <DeleteIcon />
-                </IconButton>
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() => deletaTarefa(tarefa.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Grid2>
+                <Divider />
               </Grid2>
-              <Divider />
-            </Grid2>
-          );
-        })}
-      </Paper>
-}
-      <Button
-        variant="contained"
-        color="secondary"
-        style={{ margin: "10px 0 0 0" }}
-        onClick={() =>
-          signInWithPopup(auth, provider).then((result) => {
-            console.log(result);
-          })
-        } //signInWithRedirection: redireciona sem abrir popup
-      >
-        Login
-      </Button>
-
-    </Grid2>
+            );
+          })}
+        </Paper>
+      )}
+    </Grid2> */
   );
 };
 
